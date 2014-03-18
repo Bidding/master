c2e796de6b074485065c8bd956cabf6b2d89040d
Ahmad Hattab <ahmhattab@gmail.com>
2014-03-18 14:05:15 +0200

    Pointing Module

---
+++ app/code/local/Bidding/Points/Model/Resource/Setup.php
@@ -0,0 +1,5 @@
+<?php
+class Bidding_Points_Model_Resource_Setup extends Mage_Core_Model_Resource_Setup
+{
+	
+}
---
+++ app/code/local/Bidding/Points/controllers/IndexController.php
@@ -0,0 +1,21 @@
+<?php
+class Bidding_Points_IndexController extends Mage_Core_Controller_Front_Action
+{
+	protected function _getCustomerSession()
+	{
+		$customer = Mage::getSingleton('customer/session');
+		return $customer;
+	}
+	public function indexAction()
+	{
+		$session = $this->_getCustomerSession();
+		if ($session->isLoggedIn())
+		{
+			echo 'Yes';
+		}
+		else
+		{
+			$this->_redirect('customer/account/login');
+		}
+	}
+}
