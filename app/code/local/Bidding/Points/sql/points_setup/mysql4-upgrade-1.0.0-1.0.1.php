3a071edad8e77c51c095e211fc0c48e4c4efffe1
Ahmad Hattab <ahmad.hattab@ihorizons.com>
2014-03-26 16:51:17 +0300

    Bulid entity and history table for purchasing points

--- app/code/local/Bidding/Points/sql/points_setup/mysql4-upgrade-1.0.0-1.0.1.php
+++ app/code/local/Bidding/Points/sql/points_setup/mysql4-upgrade-1.0.0-1.0.1.php
@@ -2,11 +2,13 @@
 $installer = $this;
 $installer->startSetup();
 $installer->run ("
-		DROP TABLE IF EXISTS `{$this->getTable('points/points')}`;
-         CREATE TABLE `{$this->getTable('points/points')}` (
+		DROP TABLE IF EXISTS `{$this->getTable('points/points_history')}`;
+         CREATE TABLE `{$this->getTable('points/points_history')}` (
             `id` int(11) NOT NULL AUTO_INCREMENT,
             `customer_id` int(11) NOT NULL,
-            `balance` int(11) NOT NULL DEFAULT 0,
+            `order_number` varchar(100) NOT NULL,
+            `balance` int(11) NOT NULL,
+            `date` timestamp NOT NULL,
             PRIMARY KEY (`id`)           
           ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 		");
