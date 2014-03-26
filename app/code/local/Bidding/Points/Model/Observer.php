17d5c8946d08c8cf75c659390a277b22a5171bc2
Ahmad Hattab <ahmad.hattab@ihorizons.com>
2014-03-26 18:42:58 +0300

    Create new observer for orders

---
+++ app/code/local/Bidding/Points/Model/Observer.php
@@ -0,0 +1,15 @@
+<?php 
+class Bidding_Points_Model_Observer
+{
+	public function setPoints($observer)
+	{
+		$order = $observer->getEvent()->getOrder();
+		$buyer_id = Mage::getSingleton('customer/session')->getId();
+		$quote = Mage::getSingleton('checkout/session')->getQuote();
+		$items = $quote->getAllVisibleItems();
+		echo '<pre>';
+		print_r(get_class_methods($items));
+		die;
+	}
+}
+?>
