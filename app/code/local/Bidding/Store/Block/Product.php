0f0ea1709998125df143e6a96fa0b4a118d07323
Ahmad Hattab <ahmhattab@gmail.com>
2014-04-07 17:16:16 +0300

    Bidding Store

--- app/code/local/Bidding/Store/Block/Product.php
+++ app/code/local/Bidding/Store/Block/Product.php
@@ -7,6 +7,7 @@ class Bidding_Store_Block_Product extends Mage_Core_Block_Template
 		$date = date('Y-m-d H:i:s', $currentTimestamp);
 		$products = Mage::getModel('catalog/product')->getCollection()
 					->addAttributeToSelect('*')
+					->addAttributeToFilter('can_bid', array('eq' => 1))
 					->addAttributeToFilter('start_bidding_date', array('lteq' => $date))
 					->addAttributeToFilter('end_bidding_date', array('gteq' => $date));
 		return $products;
