c02692b5632980203c025e6597cd69200e0b946e
root <root@ahmad-HP-ProBook-450-G1.(none)>
2014-04-07 11:24:42 +0300

    Bidding Store

---
+++ app/code/local/Bidding/Store/Block/Product.php
@@ -0,0 +1,14 @@
+<?php
+class Bidding_Store_Block_Product extends Mage_Core_Block_Template
+{
+	public function getProducts()
+	{
+		$currentTimestamp = Mage::getModel('core/date')->timestamp(time());
+		$date = date('Y-m-d H:i:s', $currentTimestamp);
+		$products = Mage::getModel('catalog/product')->getCollection()
+					->addAttributeToSelect('*')
+					->addAttributeToFilter('start_bidding_date', array('lteq' => $date))
+					->addAttributeToFilter('end_bidding_date', array('gteq' => $date));
+		return $products;
+	}
+}
