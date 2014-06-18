49ed36fd2480049429a2668e9157cd1f4a2fea8b
Ahmad Hattab <ahmad.hattab@ihorizons.com>
2014-06-18 18:05:59 +0300

    Force duplicate bidding on same item with same request

--- app/code/local/Bidding/Store/controllers/IndexController.php
+++ app/code/local/Bidding/Store/controllers/IndexController.php
@@ -34,65 +34,56 @@ class Bidding_Store_IndexController extends Mage_Core_Controller_Front_Action
 		if ($customerSession->isLoggedIn())
 		{
 			$data = $this->getRequest()->getParams();
-			if ($data)
+			$request = $this->getRequest()->isPost();
+			if ($request && $data['form_key'] == Mage::getSingleton('core/session')->getFormKey())
 			{
 				$points = Mage::getModel('points/points')->load($customerSession->getCustomerId(), 'customer_id');
-				$bidHistory = Mage::getModel('points/bid');
+				//$bidHistory = Mage::getModel('points/bid');
 				if ($points->getBalance() != 0)
 				{
 					$_product = Mage::getModel('catalog/product')->load($data['productId']);
-					$newPrice = $_product->getCurrentPrice() + $_product->getCpc();
+					//$newPrice = $_product->getCurrentPrice() + $_product->getCpc();
 
 					if (!$this->getTotalBid($customerSession->getCustomerId(), $data['productId']) && $this->getDiffTime($_product->getEndBiddingDate()))
 					{
 						$session = Mage::getModel('core/session');
 						$session->addError($this->__("You can't enter on this bidding becuase this is your first time and bidding will end in less than 10 minites"));
 						$data = array('action' => 'false');
-						echo json_encode($data);
+						echo "";
 					}
 					else
 					{
-						if ($newPrice < $_product->getPrice())
+						//if ($newPrice < $_product->getPrice())
+						//{
+						$bid_result = $this->setCustomerBid($customerSession->getCustomerId(), $data['productId'], $customerSession->getCustomer()->getName());
+						if (!$bid_result)
 						{
-							$bidHistory->setCustomerId($customerSession->getCustomerId());
-							$bidHistory->setProductId($data['productId']);
-							$bidHistory->setPrice($_product->getCurrentPrice());
-							$bidHistory->setNewPrice($newPrice);
-							$bidHistory->setBidDate(date("Y-m-d H:i:s", Mage::getModel('core/date')->timestamp(time())));
-							$bidHistory->save();
-							$_product->setCurrentPrice($newPrice);
-							$_product->save();
-							$points->setBalance($points->getBalance() - 1 );
-							$points->save();
-							if ($this->getDiffTime($_product->getEndBiddingDate()) && ($this->getTotalBid($customerSession->getCustomerId(), $data['productId']) < 5))
-							{
-								$session = Mage::getModel('core/session');
-								$session->addError($this->__("You should spend 5 points or more to win this bid"));
-								$data = array_merge($data, array('reload' => 'true'));
-							}
-							if ($points->getBalance() <= 10)
-							{
-								$session = Mage::getModel('core/session');
-								$session->addError($this->__("Your credit will end soon"));
-								$data = array_merge($data, array('reload' => 'true'));
-							}
-							$data = array_merge($data , array('action' => 'true','PI' => $_product->getId(), 'price'=> Mage::helper('core')->currency($_product->getCurrentPrice()), 'bidder'=> $customerSession->getCustomer()->getName(), 'bidderTable' => "<tr><td>".Mage::helper('core')->currency($_product->getCurrentPrice())."</td><td>".$customerSession->getCustomer()->getName()."</td></tr>"));
-								
+							$session = Mage::getModel('core/session');
+							$session->addError($this->__("Your bid faild becuase someone made a bid in the same time please try again now"));
+							$data = array_merge($data, array('reload' => 'true'));
 							echo json_encode($data);
+							exit;
 						}
-						elseif ($newPrice == $_product->getPrice())
+						if ($this->getDiffTime($_product->getEndBiddingDate()) && ($this->getTotalBid($customerSession->getCustomerId(), $data['productId']) < 5))
 						{
-							$bidHistory->setCustomerId($customerSession->getCustomerId());
-							$bidHistory->setProductId($data['productId']);
-							$bidHistory->setPrice($_product->getCurrentPrice());
-							$bidHistory->setNewPrice($newPrice);
-							$bidHistory->setBidDate(date("Y-m-d H:i:s", Mage::getModel('core/date')->timestamp(time())));
-							$bidHistory->save();
-							$_product->setCurrentPrice($newPrice);
-							$_product->save();
-							$points->setBalance($points->getBalance() - 1 );
-							$points->save();
+							$session = Mage::getModel('core/session');
+							$session->addError($this->__("You should spend 5 points or more to win this bid"));
+							$data = array_merge($data, array('reload' => 'true'));
+						}
+						if ($points->getBalance() <= 10)
+						{
+							$session = Mage::getModel('core/session');
+							$session->addError($this->__("Your credit will end soon"));
+							$data = array_merge($data, array('reload' => 'true'));
+						}
 							
+						$data = array_merge($data , $bid_result);
+
+						echo json_encode($data);
+						//}
+						if ($newPrice == $_product->getPrice())
+						{
+							$this->setCustomerBid($customerSession->getCustomerId(), $data['productId'], $customerSession->getCustomer()->getName());
 							$_product->setEndBiddingDate(date('Y-m-d H:i:s',strtotime("-1 days")));
 							$_product->save();
 							$session = Mage::getModel('core/session');
@@ -117,8 +108,46 @@ class Bidding_Store_IndexController extends Mage_Core_Controller_Front_Action
 		}
 		else
 		{
+			$session = Mage::getModel('core/session');
+			$session->addError($this->__("Please login to access bidding"));
 			echo "";
 		}
+	}
+
+	private function setCustomerBid($customer_id, $product_id, $customer_name)
+	{
+		$points = Mage::getModel('points/points')->load($customer_id, 'customer_id');
+		$bidHistory = Mage::getModel('points/bid');
+
+		$_product = Mage::getModel('catalog/product')->load($product_id);
+		$new_price = $_product->getCurrentPrice() + $_product->getCpc();
+
+		$bidHistory->setCustomerId($customer_id);
+		$bidHistory->setProductId($product_id);
+
+		$check_uniq_bid = Mage::getModel('points/bid')->getCollection()
+		->addFieldToFilter('product_id', array('eq' => $product_id))
+		->setOrder('new_price', 'DESC')
+		->setPageSize(1);
+		$check_uniq_bid = $check_uniq_bid->getData();
+		if ($check_uniq_bid[0]['new_price'] >= $new_price)
+		{
+			return false;
+		}
+
+		$bidHistory->setPrice($_product->getCurrentPrice());
+		$bidHistory->setNewPrice($new_price);
+		$bidHistory->setBidDate(date("Y-m-d H:i:s", Mage::getModel('core/date')->timestamp(time())));
+		$bidHistory->save();
+
+		$_product->setCurrentPrice($new_price);
+		$_product->save();
+
+		$points->setBalance($points->getBalance() - 1 );
+		$points->save();
+
+		$data = array('action' => 'true','PI' => $product_id, 'price'=> Mage::helper('core')->currency($_product->getCurrentPrice()), 'bidder'=> $customer_name, 'bidderTable' => "<tr><td>".Mage::helper('core')->currency($_product->getCurrentPrice())."</td><td>".$customer_name."</td></tr>");
+		return $data;
 	}
 
 	protected function getTotalBid($bidder_id, $product_id)
@@ -136,9 +165,11 @@ class Bidding_Store_IndexController extends Mage_Core_Controller_Front_Action
 		$interval  = abs($datetime2 - $datetime1);
 		$minutes   = round($interval / 60);
 		if ( $minutes <= 10 )
-		return true;
+			return true;
 		else
-		return false;
+			return false;
 	}
+
+
 
 }
