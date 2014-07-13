07dd4f7332ac9826dad209f1909d5952bc79bfd0
Ahmad Hattab <ahmhattab@gmail.com>
2014-07-13 20:48:50 +0300

    Add closed collection with slider in home page

--- app/code/local/Bidding/Store/Block/Product.php
+++ app/code/local/Bidding/Store/Block/Product.php
@@ -31,4 +31,16 @@ class Bidding_Store_Block_Product extends Mage_Core_Block_Template
 		$currentBidder->getSelect()->limit(1);
 		return $currentBidder->getData();
 	}
+	
+	public function getClosedBidding()
+	{
+		$currentTimestamp = Mage::getModel('core/date')->timestamp(time());
+		$date = date('Y-m-d H:i:s', $currentTimestamp);
+		$products = Mage::getModel('catalog/product')->getCollection()
+					->addAttributeToSelect('*')
+					->addAttributeToFilter('can_bid', array('eq' => 1))
+					->addAttributeToFilter('status', array('eq' => 1))
+					->addAttributeToFilter('end_bidding_date', array('lt' => $date));
+		return $products;
+	}
 }
