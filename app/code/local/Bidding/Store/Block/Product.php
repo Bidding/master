efe44bd9acacf8593ee46a3be6aeddb9dff10dde
Ahmad Hattab <ahmhattab@gmail.com>
2014-07-14 03:13:32 +0300

    Fix Bugs & add next bidding slider

--- app/code/local/Bidding/Store/Block/Product.php
+++ app/code/local/Bidding/Store/Block/Product.php
@@ -43,4 +43,17 @@ class Bidding_Store_Block_Product extends Mage_Core_Block_Template
 					->addAttributeToFilter('end_bidding_date', array('lt' => $date));
 		return $products;
 	}
+	
+	public function getNextBidding()
+	{
+		$currentTimestamp = Mage::getModel('core/date')->timestamp(time());
+		$date = date('Y-m-d H:i:s', $currentTimestamp);
+		$products = Mage::getModel('catalog/product')->getCollection()
+					->addAttributeToSelect('*')
+					->addAttributeToFilter('can_bid', array('eq' => 1))
+					->addAttributeToFilter('status', array('eq' => 1))
+					->addAttributeToFilter('start_bidding_date', array('gt' => $date));
+		$products->getSelect()->limit(3);
+		return $products;
+	}
 }
