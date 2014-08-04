48de89833dce7dcea2f07587d2a72deedd46b976
Ahmad Hattab <ahmhattab@gmail.com>
2014-08-04 23:19:09 +0300

    Add Url to bidding winner report

--- app/code/local/Bidding/Store/Block/Adminhtml/Store/Renderer/Customer.php
+++ app/code/local/Bidding/Store/Block/Adminhtml/Store/Renderer/Customer.php
@@ -5,7 +5,7 @@ Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
 	public function render(Varien_Object $row)
 	{
 		$customer = Mage::getModel('customer/customer')->load($row->getData($this->getColumn()->getIndex()));
-		$html = '<a href="#">';
+		$html = '<a href="' . Mage::helper('adminhtml')->getUrl('*/customer/edit/', array('id' => $customer->getId())) . '">';
 		$html .= $customer->getName();
 		$html .= '</a>';
 		return $html; 
--- app/code/local/Bidding/Store/Block/Adminhtml/Store/Renderer/Product.php
+++ app/code/local/Bidding/Store/Block/Adminhtml/Store/Renderer/Product.php
@@ -5,7 +5,7 @@ Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
 	public function render(Varien_Object $row)
 	{
 		$product = Mage::getModel('catalog/product')->load($row->getData($this->getColumn()->getIndex()));
-		$html = '<a href="#">';
+		$html = '<a href="' . Mage::helper('adminhtml')->getUrl('*/catalog_product/edit/', array('id' => $product->getId())) . '">';
 		$html .= $product->getName();
 		$html .= '</a>';
 		return $html;
